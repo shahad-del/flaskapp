e13481ae5004112e7bfbde290bba3ad21f75cffc
Shahad Suman <shahad.sumann@gmail.com>
2021-10-15 17:35:19 +0545

    flask app with how to include your own module in site packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name = 'vsearch',
+    version='1.0',
+    description = 'learning how to include your module in site package',
+    author = 'shahad',
+    author_email = 'shahad.sumann@gmail.com',
+    url='sitepackages.com',
+    py_modules=['vsearch']
+)
